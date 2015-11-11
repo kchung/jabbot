1c788ac047754fda1ba0e6e08fbce9ca886c59d6
Kevin Chung <kevin@weebly.com>
2015-11-10 19:18:35 -0800

    Remove shorthand `st` as a trigger word

--- src/Jabbot.js
+++ src/Jabbot.js
@@ -13,7 +13,7 @@ export default class Jabbot extends Slackbots {
    * @property {String} Regex to detect if a Jabwire short hand is posted
    *   like "Bug #1234" and "Sprint Task #12312"
    */
-  shorthand = '(bug|sprint(?:\\s+)?task|st)(?:\\s+)?(?:#)?(\\d+)';
+  shorthand = '(bug|sprint(?:\\s+)?task)(?:\\s+)?(?:#)?(\\d+)';
 
   /**
    * @property {String} Jabwire URL schema
